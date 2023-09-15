862b25336c06f6ddbc293de100cf4ab754d4de06
sofyan kirat <www.sofyankirat123@gmail.com>
2023-09-15 11:49:27 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always  (Success)
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+script 1: Write a program that prints _putchar, followed by a new line.
+The program should return 0
