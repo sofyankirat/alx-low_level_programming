cf19d23f7d68dacd06a5115d9b08389e8d66bf3f
sofyan kirat <www.sofyankirat123@gmail.com>
2023-09-16 01:34:39 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,15 +7,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _islower(int c)
+int _islower(int c);
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (0);
-	}
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
+	else
+		return (0);
 	return (0 || 1);
 }
