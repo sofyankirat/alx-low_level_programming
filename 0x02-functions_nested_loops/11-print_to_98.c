38b0b4126da1878f6e1f1193c570e81ad49e474a
sofyan kirat <www.sofyankirat123@gmail.com>
2023-09-17 16:19:01 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,15 +15,10 @@ void print_to_98(int n)
 		printf("%i", n);
 		printf(", ");
 	}
-	for (; n < 98; n++)
+	for (; n <= 98; n++)
 	{
 		printf("%i", n);
+		if (n != 98)
 		printf(", ");
-	}
-	if (n == 98)
-	{
-		_putchar('0' + n / 10);
-		_putchar('0' + n % 10);
-		_putchar('\n');
 	}
 }
