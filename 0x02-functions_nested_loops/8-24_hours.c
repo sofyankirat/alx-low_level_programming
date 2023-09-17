705bd43bb015ec75fcd410333a149320adb420dc
sofyan kirat <www.sofyankirat123@gmail.com>
2023-09-16 18:09:18 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -27,37 +27,26 @@ void jack_bauer(void)
 				int l = 0;
 
 				if (k != 0)
-				{
-					_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(':');
-				}
+				{_putchar('0' + i);
+				_putchar('0' + j);
+				_putchar(':'); }
 				_putchar('0' + k);
 				while (l <= 9)
-				{
-					_putchar('0' + l);
-					_putchar('\n');
-					if (k == 5 && l == 9)
-						break;
-					if (l != 9)
-					{
-						_putchar('0' + i);
-						_putchar('0' + j);
-						_putchar(':');
-						_putchar('0' + k);
-					}
-					l++;
-				}
-				k++;
-			}
+				{_putchar('0' + l);
+				_putchar('\n');
+				if (k == 5 && l == 9)
+					break;
+				if (l != 9)
+				{_putchar('0' + i);
+				_putchar('0' + j);
+				_putchar(':');
+				_putchar('0' + k); }
+				l++; }
+				k++; }
 			if (i == 2 && j == 3)
 				break;
 			if (j != 9)
-			{
-				_putchar('0' + i);
-			}
-			j++;
-		}
-		i++;
-	}
+			{_putchar('0' + i) ; }
+			j++; }
+		i++; }
 }
