8c793f795ca34abd94fdb9fea7c305899ea964e0
sofyan kirat <www.sofyankirat123@gmail.com>
2023-09-19 09:15:18 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,9 +13,7 @@ int main(void)
 {
 	long pro1, pro2 = 2, pro3 = 1, sum1 = 0, sum2 = 0, sum3 = 0, i = 1;
 
-	printf("1, ");
-	printf("2, ");
-	while (i <= 16)
+	while (i <= 10)
 	{
 		pro1 = pro2 + pro3;
 		if (pro1 % 2 == 0)
