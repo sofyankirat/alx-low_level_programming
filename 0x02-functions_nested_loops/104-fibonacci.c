39877b70831c4a1c7118f065261edecd258b956f
sofyan kirat <www.sofyankirat123@gmail.com>
2023-09-19 12:54:25 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,26 +12,32 @@
 
 int main(void)
 {
-	unsigned long pro1, pro2 = 2, pro3 = 1, i = 1;
+	unsigned long pro1, pro2 = 2, pro3 = 1, i = 1, h1, h2, h3, h4, h5, h6;
 
 	printf("1, ");
 	printf("2, ");
 	while (i <= 32)
-	{
-		pro1 = pro2 + pro3;
-		printf("%lu, ", pro1);
+	{pro1 = pro2 + pro3;
+		if (i <= 30)
+		{printf("%lu, ", pro1); }
+		else if (i > 30)
+		{h1 = pro1 / 10000000000;
+			h2 = pro1 % 10000000000;
+			printf("%lu%lu, ", h1, h2); }
 		pro3 = pro1 + pro2;
-		printf("%lu, ", pro3);
+		if (i <= 30)
+		{printf("%lu, ", pro3); }
+		else if (i > 30)
+		{h3 = pro3 / 10000000000;
+			h4 = pro3 % 10000000000;
+			printf("%lu%lu, ", h3, h4); }
 		pro2 = pro3 + pro1;
-		if (i != 32)
-		{
-			printf("%lu, ", pro2);
-		}
-		else
-		{
-			printf("%lu\n", pro2);
-			break;
-		}
+		if (i <= 30)
+		{printf("%lu, ", pro2); }
+		else if (i > 30)
+		{h5 = pro2 / 10000000000;
+			h6 = pro2 % 10000000000;
+			printf("%lu%lu, ", h5, h6); }
 		i++;
 	}
 	return (0);
