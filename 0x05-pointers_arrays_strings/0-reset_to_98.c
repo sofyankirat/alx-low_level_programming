bfb3088604d3660d9a0ab28fd7aac637583b5d7e
sofyan kirat <www.sofyankirat123@gmail.com>
2023-09-22 07:35:36 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value.
- * @*n: The integer to print
+ * @n: The integer to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
