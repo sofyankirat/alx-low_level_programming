0e8d535ded44165999ca286b2b696651fa98bf29
sofyan kirat <www.sofyankirat123@gmail.com>
2023-10-08 12:30:47 -0700

    Trust no one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
 
-#ifndef
+#endif
