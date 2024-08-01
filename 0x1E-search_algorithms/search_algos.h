d8b1e4e86d5a2d41e83651541ae5b0fd91f3df2b
sofyankirat <www.sofyankirat123@gmail.com>
2024-08-01 08:41:08 -0700

    cs

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _search_algos_
+#define _search_algos_
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t l, size_t r);
+int binary_search_index(int *array, size_t l, size_t r, int value);
+
+#endif /* _search_algos_ */
